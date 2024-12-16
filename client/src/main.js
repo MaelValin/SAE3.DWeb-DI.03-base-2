5d6f1c0477a5b5d6feff68e4347efa0f35e9b061
MaelValin <maelmy19@gmail.com>
2024-12-16 11:37:13 +0100

    itération 3

--- client/src/main.js
+++ client/src/main.js
@@ -12,6 +12,9 @@ C.init = async function(){
     console.log(Lycees.getAll());
 }
 
+
+
+
 let V = {
     header: document.querySelector("#header")
 };
@@ -23,6 +26,57 @@ V.init = function(){
 V.renderHeader= function(){
     V.header.innerHTML = HeaderView.render();
 }
+
+
+
+var map = L.map('map').setView([45.83,1.26], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//methode a utiliser
+
+var marker = L.marker([45.83,1.26]).addTo(map);
+
+var circle = L.circle([45.83,1.26], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+
+/*var polygon = L.polygon([
+    [45.85,1.22],
+    [45.87,1.26],
+    [45.83,1.29]
+]).addTo(map);*/
+
+
+/*marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");*/
+
+/*var popup = L.popup()
+    .setLatLng([45.83,1.26])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+    var popup = L.popup();*/
+
+
+
+/*    function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);*/
+
 
 
 C.init();
